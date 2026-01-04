208955c3426253209e9ca0520cd16a16cb94b576
Hugo Ramon Pereira <hramonp-dev@outlook.com>
2026-01-03 21:15:09 -0300

    Added hello use case and applied dependency injection

--- src/application/controllers/HelloController.ts
+++ src/application/controllers/HelloController.ts
@@ -1,16 +1,23 @@
 import { Controller } from "@aplication/contracts/Controller";
 import { Schema } from "@kernel/decorators/Schema";
+import { HelloUseCase } from "../../usecases/HelloUseCase";
 import { HelloBody, helloSchema } from "./schemas/helloSchema";
 
 @Schema(helloSchema)
 export class HelloController extends Controller<unknown> {
+  constructor(private readonly helloUseCase: HelloUseCase) {
+    super();
+  }
   protected override async handle(
     request: Controller.Request<HelloBody>
   ): Promise<Controller.Response<unknown>> {
+    const result = await this.helloUseCase.execute({
+      email: request.body.email,
+    });
     return {
       statusCode: 200,
       body: {
-        parsedBody: request.body,
+        result,
       },
     };
   }
--- src/main/functions/hello.ts
+++ src/main/functions/hello.ts
@@ -2,7 +2,8 @@ import "reflect-metadata";
 
 import { HelloController } from "@aplication/controllers/HelloController";
 import { lambdaHttpAdapter } from "@main/adapters/lambdaHttpAdapter";
+import { HelloUseCase } from "../../usecases/HelloUseCase";
 
-const controller = new HelloController();
+const controller = new HelloController(new HelloUseCase());
 
 export const handler = lambdaHttpAdapter(controller);
