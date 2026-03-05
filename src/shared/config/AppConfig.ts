631703d1d4fc206ba0f81f4dcf4ec3b527d6cebb
Hugo Ramon Pereira <hramonp-dev@outlook.com>
2026-03-04 23:46:17 -0300

    Added fix for file input url

--- src/shared/config/AppConfig.ts
+++ src/shared/config/AppConfig.ts
@@ -6,6 +6,7 @@ export class AppConfig {
   readonly auth: AppConfig.Auth;
   readonly db: AppConfig.Database;
   readonly storage: AppConfig.Storage;
+  readonly cdn: AppConfig.CDN;
 
   constructor() {
     this.auth = {
@@ -28,6 +29,10 @@ export class AppConfig {
 
     this.storage = {
       mealsBucket: env.MEALS_BUCKET,
+    };
+
+    this.cdn = {
+      mealsCDN: env.MEALS_CDN_DOMAIN_NAME,
     };
   }
 }
@@ -53,5 +58,9 @@ export namespace AppConfig {
 
   export type Storage = {
     mealsBucket: string;
+  };
+
+  export type CDN = {
+    mealsCDN: string;
   };
 }
